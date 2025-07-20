649d367488fd5d30983f653f9a93fbe748a805aa
rezniki <igorreznik2001@gmail.com>
2025-07-20 15:00:24 +0300

    Main.ts / connect Pinia and call data upload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/dist/vuetify.min.css'; // Импортируем стили Vuetify
+import '@mdi/font/css/materialdesignicons.css'; // Иконки Material Design
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(pinia);
+app.use(vuetify);
+
+app.mount('#app');
